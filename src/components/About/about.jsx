1b6fa50b20c46a0cec2e1673884084ef2f14cc68
itsdamel <mel.mayara2014@gmail.com>
2023-10-28 12:25:02 +0200

    Handling display of  extra personal info

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,9 +1,22 @@
 
+import { useState } from 'react';
 import SectionTitle from '../SectionTitle/sectionTitle';
 
 import style from './about.module.css';
 
 export default function About(){
+    const [showMore, setShowMore] = useState(false);
+    const moreContent = " people say I’m a nice person to be around. I love Lego, board games and watching volleyball, if that’s not a crazy enough mixture for you, I also enjoy renewing old forniture!"
+
+    const preview = () => {
+        return(
+            <img onClick={handleShowMore} id={style.readMore} src='assets/left-arrow.png' alt='read more'/>
+        )
+    }
+
+
+    const handleShowMore = () => setShowMore(!showMore);
+
     return(
         <section className={style.aboutSection}>
             <a href='#text'><img id={style.arrowDown} src='assets/arrow-down.png' alt="go to about me"/></a>
@@ -25,7 +38,7 @@ export default function About(){
                     <div id={style.aboutp}>
                         <p>I’m a enthusiastic long-life student <span>passionate about problem solving</span>. Whenever I think about a new idea, planning how to translate it into code is my second favorite part, the first one is coding it! </p>
                         <p>Thus, I’m <span>currently pursuing a software engineer bachelor’s degree </span>. For the past two years, I’ve been dedicating every single day of the week to learn how to write code that not only works, but that optimizes my applications and give users a better experience.</p>
-                        <p id={style.personal}>Outside of work...<img id={style.readMore} src='assets/left-arrow.png' alt='read more'/></p>
+                        <p id={style.personal}>Outside of work...{showMore&&moreContent}{<img onClick={handleShowMore} id={showMore?style.readLess:style.readMore} src='assets/left-arrow.png' alt={showMore?'read less':'read more'}/>}</p>
                     </div>
             </div>
 
