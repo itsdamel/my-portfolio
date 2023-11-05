0125d2930686990c4800df1fcee97125250c8d0b
itsdamel <mel.mayara2014@gmail.com>
2023-11-05 13:52:32 +0100

    checkinf for misspelling

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -5,7 +5,7 @@ import style from './about.module.css';
 
 export default function About(){
     const [showMore, setShowMore] = useState(false);
-    const moreContent = "people say I’m a nice person to be around. I love Lego, board games and watching volleyball, if that’s not a crazy enough mixture for you, I also enjoy renewing old forniture!"
+    const moreContent = "People say I'm a nice person to be around. I love Lego, board games, and watching volleyball. If that's not a crazy enough mixture for you, I also enjoy renewing old furniture!"
 
     const handleShowMore = () => setShowMore(!showMore);
 
@@ -30,7 +30,7 @@ export default function About(){
                     <SectionTitle title='about'/>
                     <div id={style.aboutp}>
                         <p>I’m a enthusiastic long-life student <span>passionate about problem solving</span>. Whenever I think about a new idea, planning how to translate it into code is my second favorite part, the first one is coding it! </p>
-                        <p>Thus, I’m <span>currently pursuing a software engineer bachelor’s degree </span>. For the past two years, I’ve been dedicating every single day of the week to learn how to write code that not only works, but that optimizes my applications and give users a better experience.</p>
+                        <p>Therefore, I’m <span>currently pursuing a bachelor's degree in software engineering</span>. For the past two years, I’ve been dedicating every single day of the week to learn how to write code that not only works, but that also optimizes my applications and give users an excellent experience.</p>
                         <p id={style.personal}>Outside of work...{showMore&&moreContent}{<img onClick={handleShowMore} id={showMore?style.readLess:style.readMore} src='assets/left-arrow.png' alt={showMore?'read less':'read more'}/>}</p>
                     </div>
             </div>
